017d93a1ff1a6fdfa50e85d22b901b6f1c76a5e7
Paweł Bańka <pawel@pawelbanka.net>
2020-05-13 10:58:35 +0200

    Even more minor cleanup

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -84,8 +84,7 @@ Yankee Hardware 110 Deg. Hinge;COM-123908;WH-A,10|WH-B,11
 Hdw Accuride CB0115-CASSRC - Locking Handle Kit - Black;CB0115-CASSRC;WH-C,13|WH-B,5
 Veneer - Charter Industries - 3M Adhesive Backed - Cherry 10mm - Paper Back;3M-Cherry-10mm;WH-A,10|WH-B,1
 Veneer - Cherry Rotary 1 FSC;COM-123823;WH-C,10
-MDF, CARB2, 1 1/8";COM-101734;WH-C,8
-""")
+MDF, CARB2, 1 1/8";COM-101734;WH-C,8""")
 
 [<EntryPoint>]
 let main argv =
